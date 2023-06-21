4571e284d56f202729379a01253037d022517fae
khoahocgioi9 <74848070+khoahocgioi9@users.noreply.github.com>
2023-06-21 20:41:33 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Navbar = () => {
+  return (
+    <div className="underline">Navbar</div>
+  );
+};
+
+export default Navbar;
