6c11e34778b321b2e6d8adb459668452fcdb0ece
bsdaoquang <bsdaoquang@gmail.com>
2023-06-21 21:46:55 +0700

    add layout

--- src/App.js
+++ src/App.js
@@ -3,50 +3,63 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import ErrorPage from "./pages/ErrorPage";
-import {Home} from "./pages/Home";
-import {TopArtists} from "./pages/TopArtists";
-import {TopChart} from "./pages/TopChart";
+import { Home } from "./pages/Home";
+import { TopArtists } from "./pages/TopArtists";
+import { TopChart } from "./pages/TopChart";
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Layout } from 'antd';
 
-const Layout =() => {
+const { Sider, Content, Header } = Layout
+
+// const Layout = () =>
+// {
+//   return (
+//     <div className='app'>
+//       <Navbar />
+//       <Outlet />
+//       <Sidebar />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "*",
+//         element: <ErrorPage />
+//       },
+//       {
+//         path: "/",
+//         element: <Home />
+//       },
+//       {
+//         path: "/top-artists",
+//         element: <TopArtists />
+//       },
+//       {
+//         path: "/top-chart",
+//         element: <TopChart />
+//       },
+//     ]
+//   }
+// // ])
+
+function App()
+{
   return (
-    <div className='app'>
-      <Navbar/>
-      <Outlet/>
-      <Sidebar/>
-      <Footer/>
-    </div>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout/>,
-    children: [
-      {
-        path: "*",
-        element: <ErrorPage/>
-      },
-      {
-        path:"/",
-        element: <Home/>
-      },
-      {
-        path:"/top-artists",
-        element: <TopArtists/>
-      },
-      {
-        path:"/top-chart",
-        element: <TopChart/>
-      },
-    ]
-  }
-])
-
-function App() {
-  return (
-    <RouterProvider router={router}/>
+    <Layout>
+      <Sidebar />
+      <Layout>
+        <Header />
+        <Content>
+          <h1>Hello world!</h1>
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
